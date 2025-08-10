0f0a64075f43685cfed7d031081ea363d4a14aac
Julian Beraun <El_Jbc_99@hotmail.com>
2025-08-10 19:05:13 -0300

    agregado mainMenu route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainMenuButtonComponent } from './components/main-menu-button/main-menu-button.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 export const routes: Routes = [
     { path: 'start', pathMatch: 'full', component: MainMenuButtonComponent},
+    { path: 'mainMenu', pathMatch: 'full', component: MainMenuComponent},
 ];
